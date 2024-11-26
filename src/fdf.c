a520ec5de24149c505b907017491ba415658bf86
ldel-val <ldel-val@student.42madrid.com>
2024-11-26 15:56:17 +0100

    emptied fdf.c because i will use sandbox.c to test things instead

--- src/fdf.c
+++ src/fdf.c
@@ -6,14 +6,12 @@
 /*   By: ldel-val <ldel-val@42madrid.com>          |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/11/25 16:43:50 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/11/25 16:45:17 by ldel-val          ``                     */
+/*   Updated: 2024/11/26 15:56:11 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-char *test();
 
 int main(void)
 {
-	printf("%s\n", test());
 }
