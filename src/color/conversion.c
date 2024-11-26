75ef265e451690984317c65ba8ccd9f8d98b54cc
ldel-val <ldel-val@student.42madrid.com>
2024-11-26 15:51:00 +0100

    fixed rgbaatoi

--- src/color/conversion.c
+++ src/color/conversion.c
@@ -6,22 +6,22 @@
 /*   By: ldel-val <ldel-val@42madrid.com>          |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/11/25 16:45:42 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/11/26 12:32:57 by ldel-val          ``                     */
+/*   Updated: 2024/11/26 14:50:02 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fdf.c>
+#include "../fdf.h"
 
-int *rgbtoi(t_rgb rgb)
+int	rgbtoi(t_rgba rgba)
 {
 	int color;
 
-	color = rgb.a;
-	color << 8;
-	color += rbg.r;
-	color << 8;
-	color += rbg.g;
-	color << 8;
-	color += rbg.b;
+	color = rgba.a;
+	color = color << 8;
+	color += rgba.r;
+	color = color << 8;
+	color += rgba.g;
+	color = color << 8;
+	color += rgba.b;
 	return (color);
 }
