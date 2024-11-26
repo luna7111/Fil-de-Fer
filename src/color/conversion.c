5993c15d5b42331a79ad1743f7c0b534d1e6af28
ldel-val <ldel-val@student.42madrid.com>
2024-11-26 12:33:12 +0100

    Added a function to convert from rbga to the strange mlx color format

--- src/color/conversion.c
+++ src/color/conversion.c
@@ -6,13 +6,22 @@
 /*   By: ldel-val <ldel-val@42madrid.com>          |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/11/25 16:45:42 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/11/25 19:25:50 by ldel-val          ``                     */
+/*   Updated: 2024/11/26 12:32:57 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.c>
 
-int *rgbtoi(t_rgb color)
+int *rgbtoi(t_rgb rgb)
 {
+	int color;
 
+	color = rgb.a;
+	color << 8;
+	color += rbg.r;
+	color << 8;
+	color += rbg.g;
+	color << 8;
+	color += rbg.b;
+	return (color);
 }
