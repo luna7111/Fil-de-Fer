985b3e5e54c332557609d1d83069d98acb4fad95
Luna del Valle <luna7111@tuta.io>
2024-12-01 23:22:23 +0100

    Modified to include new header

--- src/fdf.h
+++ src/fdf.h
@@ -6,16 +6,17 @@
 /*   By: ldel-val <ldel-val@42madrid.com>          |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/11/26 12:34:16 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/11/29 15:32:45 by ldel-val          ``                     */
+/*   Updated: 2024/12/01 12:19:00 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-//# include <mlx.h>
+# include "../mlx/mlx.h"
 # include "../libft/libft.h"
-# include "color/color.h"
 
+# include "color/color.h"
+# include "parse/maps/maps.h"
 
 #endif
