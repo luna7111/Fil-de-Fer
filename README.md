d0d3414a47687e267576ed23aa9b019996353e66
Luna <luna7111@tuta.io>
2024-12-25 20:02:11 +0100

    Updated readme.md with a new screenshot

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ FdF is a project inside the 42 Common core. It consists on the developing of a t
 
 ## Files:
 FdF maps should be described in a plain text file where any point in the map is written as a number, separated with spaces in a grid.
-### Example:
+### For example this map:
+
 ![Screenshot1](https://github.com/luna7111/fdf/blob/main/images/basic_grid_file_screenshot.png?raw=true "basic_grid.fdf screenshot")
+
+### Should look something like:
+
+![Screenshot2](https://github.com/luna7111/fdf/blob/main/images/basic_grid_render_screenshot.png?raw=true "basic_grid.fdf render screenshot")
--- src/projection/isometric.c
+++ src/projection/isometric.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2024/12/11 14:25:33 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2024/12/22 18:15:34 by ldel-val          ``                     */
+/*   Updated: 2024/12/25 19:57:47 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_2d_point	iso_project(t_3d_point point, t_data data)
 	point.z *= line_multiplier;
 	projected.color = point.color;
 	projected.y = data.cam_y + (data.win_h / 3);
-	projected.y -= (point.z / 20) + (point.x * sin(60)) + (point.y * sin(60));
+	projected.y -= (point.z / 5) + (point.x * sin(60)) + (point.y * sin(60));
 	projected.x = data.cam_x + (data.win_w / 2);
 	projected.x += (point.y * cos(60)) - (point.x * cos(60));
 	return (projected);
